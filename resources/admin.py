1aeffd42829aa14bb1d511b2903e672144e20376
David H <129187448+Git-Lister@users.noreply.github.com>
2026-01-22 16:29:56 +0000

    "Admin neaten up - again"

--- resources/admin.py
+++ resources/admin.py
@@ -157,18 +157,20 @@ class HasEmbeddingsFilter(admin.SimpleListFilter):
 
     def lookups(self, request, model_admin):
         return [
-            ("yes", "All resources have embeddings"),
-            ("no", "Some resources missing embeddings"),
+            ("yes", "Resources with embeddings"),
+            ("no", "Resources without embeddings"),
         ]
+
 
     def queryset(self, request, queryset):
         if self.value() is None:
             return queryset
 
         qs = queryset.annotate(
-            total_resources=Count("oerresource"),
-            with_embeddings=Count("oerresource", filter=Q(oerresource__content_embedding__isnull=False)),
+            total_resources=Count("resources"),
+            with_embeddings=Count("resources", filter=Q(resources__content_embedding__isnull=False)),
         ).filter(total_resources__gt=0)
+
 
         if self.value() == "yes":
             return qs.filter(total_resources=F("with_embeddings"))
@@ -301,6 +303,7 @@ class OERSourceAdmin(admin.ModelAdmin):
             '''
             <a class="button" href="{}" style="background-color: #417690; color: white; 
             padding: 5px 10px; text-decoration: none; border-radius: 3px; margin-right: 5px;">🌾 Harvest</a>
+            <br>
             <a class="button" href="{}" style="background-color: #28a745; color: white; 
             padding: 5px 10px; text-decoration: none; border-radius: 3px;">🧪 Test</a>
             ''',
